6b26a574dfad225210e4e3564beb8c7f6cfc5a2c
BayoDev <bayodev@users.noreply.github.com>
2025-03-26 17:09:53 +0100

    Branch working with synth compatible design

--- no_pipeline_core/IF/instruction_fetch.v
+++ no_pipeline_core/IF/instruction_fetch.v
@@ -1,18 +1,12 @@
 module instruction_fetch(
-
-    input clk,rst,
-
-    input [31:0]    branch_result,
-    input           was_branch,
-
-    output reg [31:0]   instr_out,
-
-    // Propagation
-    output reg [31:0]       pc_propagation
-
+    input clk, rst,
+    input [31:0] branch_result,
+    input was_branch,
+    output reg [31:0] instr_out,
+    output reg [31:0] pc_propagation
 );
 
-wire [31:0] instr_wire_out,pc_prop_wire;
+wire [31:0] instr_wire_out, pc_prop_wire;
 
 pc_register pc(
     .clk(clk),
@@ -25,24 +19,18 @@ pc_register pc(
 instruction_memory mem(
     .clk(clk),
     .rst(rst),
-    .address(pc_prop_wire),
+    .address(was_branch ? branch_result : pc_prop_wire), // Use branch_result immediately if was_branch is true
     .data_out(instr_wire_out)
 );
 
 always @(posedge clk or posedge rst) begin
-    if(rst) begin
+    if (rst) begin
         instr_out <= 32'b0;
         pc_propagation <= 32'b0;
-    end
-    else begin
+    end else begin
         instr_out <= instr_wire_out;
-        pc_propagation <= pc_prop_wire;
+        pc_propagation <= was_branch ? branch_result : pc_prop_wire; // Update pc_propagation accordingly
     end
-    // $display("[IF-PHASE] PC: 0x%08h INSTR: 0x%08h",data_out,instr_out);
 end
-
-// always @(negedge clk)begin
-    
-// end
 
 endmodule
--- no_pipeline_core/IF/pc_register.v
+++ no_pipeline_core/IF/pc_register.v
@@ -1,21 +1,18 @@
 module pc_register(
-    input clk,rst,
-
-    input [31:0]       data_in,
-    input              write_en,
-    output reg [31:0]  data_out
+    input clk, rst,
+    input [31:0] data_in,
+    input write_en,
+    output reg [31:0] data_out
 );
 
 always @(posedge clk or posedge rst) begin
-    if(rst)
+    if (rst) begin
         data_out <= 32'b0;
-    else 
-        data_out <=  data_out + 4;
-end
-
-always @(negedge clk) begin
-    if(!rst && write_en)
-        data_out <= data_in;
+    end else if (write_en) begin
+        data_out <= data_in + 4; 
+    end else begin
+        data_out <= data_out + 4; 
+    end
 end
 
 endmodule
